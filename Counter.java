a08e86c4bb26b9fb68a391d726255546628a9282
sysyishui <145036157+sysyishui@users.noreply.github.com>
2024-09-11 18:42:21 -0400

    Counter.java

--- Counter.java
+++ Counter.java
@@ -130,7 +130,7 @@ public class Counter
         // CHANGE THE RETURN TO A DESCRIPTION OF THE COUNTER
         // MUST FOLLOW THE FOLLOWING FORMAT:
         //Counter: value=1 min=1 max=9 rolled over=false
-	return "Counter: value= " + value() + " min= " + min + " max= " + max + " rolled over= " + rolledOver();
+	return "Counter: value=" + value() + " min=" + min + " max=" + max + " rolled over=" + rolledOver();
     }
  
 }
